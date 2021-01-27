530ff7dab22ba968a7be1f3673997a452fc63973
imperiordragon <aayushs2797@gmail.com>
2021-01-28 01:30:56 +0530

    Add styled components

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import AboutSection from "../components/AboutSection";
-
-import ServicesSection from "../components/ServicesSection";
 
 const FaqSection = () => {
     return(
